1d0dd27d0009dd79a34ceb3495ee3ce5486b7494
ZetaKraken <edumax526@gmail.com>
2022-04-14 10:53:16 -0400

    update14{

--- vistas/modulos/practicante.php
+++ vistas/modulos/practicante.php
@@ -37,19 +37,20 @@
         <table id="tablaPracticante" class="table table-striped table-bordered nowrap" style="width:100%;">
             <thead class="bg-info">
                 <tr>
-                    <td style="width:5%;">Id</td>
-                    <td style="width:5%;">Nombres</td>
-                    <td style="width:5%;">Apellidos</td>
-                    <td style="width:5%;">RUT</td>
-                    <td style="width:5%;">Institucion</td>
-                    <td style="width:5%;">Carrera</td>
-                    <td style="width:5%;">Tipo de Practica</td>
-                    <td style="width:2%;">Fecha Inicio</td>
-                    <td style="width:2%;">Fecha Termino</td>
-                    <td style="width:5%;">Foto</td>
-                    <td style="width:5%;">Encargado</td>
-                    <td style="width:5%;">Acciones</td>
+                    <td style="width:1%;">Id</td>
+                    <td style="width:1%;">Nombres</td>
+                    <td style="width:1%;">Apellidos</td>
+                    <!-- <td style="width:1%;">RUT</td> -->
+                    <td style="width:1%;">Institucion</td>
+                    <td style="width:1%;">Carrera</td>
+                    <td style="width:1%;">Tipo de Practica</td>
+                    <td style="width:1%;">Inicio</td>
+                    <td style="width:1%;">Termino</td>
+                    <!-- <td style="width:1%;">Foto</td>
+                    <td style="width:1%;">Encargado</td> -->
+                    <td style="width:1%;">Acciones</td>
                 </tr>
+
             </thead>
             <tbody>
             </tbody>
@@ -96,10 +97,21 @@
                             <input type="text" class="form-control" name="apellidos" id="txtapellidos" placeholder="Ingrese el apellido">
                         </div>
                    </div>
-                   <div class="col-sm-4">
+                   <!-- <div class="col-sm-4">
                         <div class="form-group">
                             <label for="txtrut">Rut</label>
                             <input type="text" class="form-control" name="rut"    id="txtrut" placeholder="Ingrese el rut">
+                        </div>
+                   </div> -->
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <form>
+                        <button type="submit">Validador RUT</button>
+                           
+                            <input type="text" class="form-control" id="rut" name="rut" required oninput="checkRut(this)" placeholder="Ingrese RUT">
+                              
+                         <!-- <script src="validarRUT.js"></script> -->
+                </form>
                         </div>
                    </div>
                    <div class="col-sm-4">
@@ -245,22 +257,264 @@
     </div>
 
 </div>
-<!-- ./ VENTANA MODAL PARA REGISTRO Y ACTUALIZACION -->
+<!-- VENTANA MODAL VISUALIZAR -->
+<div class="modal fade" id="modal-gestionar-pver">
 
+    <div class="modal-dialog modal-lg">
+
+        <div class="modal-content">
+
+            <!-- ============================================================
+            =MODAL HEADER
+            ===============================================================-->
+            <div class="modal-header bg-info">
+                <h4 class="modal-title">Visualizacion de Datos del Practicante</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <!-- ============================================================
+            =MODAL BODY
+            ===============================================================-->
+            <div class="modal-body">
+               <div class="row">
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                            <input type="hidden" id ="idpracticante" name ="practicantes" value ="">
+
+                            <label for="txtnombres">Nombres</label>
+                            <input type="text" class="form-control" name="nombres" id="txtnonbre" placeholder="Ingrese el nombre">
+                        </div>
+                   </div>
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="txtapellidos">Apellidos</label>
+                            <input type="text" class="form-control" name="apellidos" id="txtapellido" placeholder="Ingrese el apellido">
+                        </div>
+                   </div>
+                
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <form>
+                        <button type="submit">Validador RUT</button>
+                           
+                            <input type="text" class="form-control" id="ru" name="rut" required oninput="checkRut(this)" placeholder="Ingrese RUT">
+                    
+                </form>
+                        </div>
+                   </div>
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <label for="fecha_inicio">Fecha Inicio</label>
+	                    <input type="date" class="form-control" id="fecha_inici" name="fecha_inicio" placeholder="aaaa-mm-dd">
+                        </div>
+                   </div>
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                       
+                        <label for="fecha_termino">Fecha Termino</label>
+                        <input type="date" class="form-control" id="fecha_termin" name="fecha_termino" placeholder="aaaa-mm-dd">
+                        </div>
+                   </div>
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <label for="carrera">Seleccione Carrera:</label>
+                        <select class="form-control" id="Scarrer">
+                        <option value="">Ninguna</option>
+                        <?php
+                        $usuario = 'root';
+                        $password = '';
+                        $db = new PDO('mysql:host=localhost;dbname=db_practicantes', $usuario, $password,
+                        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+                        $query = $db->prepare("SELECT * FROM carreras");
+                        $query->execute();
+                        $data = $query->fetchAll();
+
+                        foreach ($data as $valores):
+                        echo '<option value="'.$valores["ID_CARRERA"].'">'.$valores["NOMBRE"].'</option>';
+                        endforeach;
+                        ?>
+                        </select>
+                    
+                        </div>
+                   </div>
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <label for="carrera">Seleccione Institucion:</label>
+                        <select class="form-control" id="Sinstitucio" >
+                        <option value="">Ninguna</option>
+                        <?php
+                        $usuario = 'root';
+                        $password = '';
+                        $db = new PDO('mysql:host=localhost;dbname=db_practicantes', $usuario, $password,
+                        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+                        $query = $db->prepare("SELECT * FROM instituciones");
+                        $query->execute();
+                        $data = $query->fetchAll();
+
+                        foreach ($data as $valores):
+                        echo '<option value="'.$valores["ID_INSTITUCION"].'">'.$valores["NOMBRE"].'</option>';
+                        endforeach;
+                        ?>
+                        </select>
+                    
+                        </div>
+                   </div>
+
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <label for="carrera">Seleccione Encargado:</label>
+                        <select class="form-control" id="Sencargad" >
+                        <option value="">Ninguna</option>
+                        <?php
+                        $usuario = 'root';
+                        $password = '';
+                        $db = new PDO('mysql:host=localhost;dbname=db_practicantes', $usuario, $password,
+                        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+                        $query = $db->prepare("SELECT * FROM encargados");
+                        $query->execute();
+                        $data = $query->fetchAll();
+
+                        foreach ($data as $valores):
+                        echo '<option value="'.$valores["ID_ENCARGADO"].'">'.$valores["NOMBRE"].'</option>';
+                        endforeach;
+                        ?>
+                        </select>
+                    
+                        </div>
+                   </div>
+
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                        <label for="carrera">Seleccione Tipo de Practica:</label>
+                        <select class="form-control" name="Stipo_practica" id="Stipo_practic">
+                        <option value="">Ninguna</option>
+                        <?php
+                        $usuario = 'root';
+                        $password = '';
+                        $db = new PDO('mysql:host=localhost;dbname=db_practicantes', $usuario, $password,
+                        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+                        $query = $db->prepare("SELECT * FROM tipo_practicaS");
+                        $query->execute();
+                        $data = $query->fetchAll();
+                        
+                        foreach ($data as $valores):
+                        echo '<option value="'.$valores["ID_TIPO_PRACTICA"].'">'.$valores["NOMBRE"].'</option>';
+                        endforeach;
+                        
+                        ?>
+                        </select>
+                    
+                        </div>
+                       
+                   </div>
+                   <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="txtfoto">Foto</label>
+                            <input type="text" class="form-control" name="foto"    id="txtfot" placeholder="Ingrese la foto">
+                        </div>
+                   </div>
+
+
+
+
+
+                   
+
+
+
+               </div>
+            </div>
+            <!-- ============================================================
+            =MODAL FOOTER
+            ===============================================================-->
+            <div class="modal-footer justify-content-end">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Dejar de Ver</button>
+             
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<!-- ./ VENTANA VERIFICAR RUT -->
+<script>
+        function checkRut(rut) {
+            // Despejar Puntos
+            var valor = rut.value.replace('.','');
+            // Despejar Guión
+            valor = valor.replace('-','');
+            
+            // Aislar Cuerpo y Dígito Verificador
+            cuerpo = valor.slice(0,-1);
+            dv = valor.slice(-1).toUpperCase();
+            
+            // Formatear RUN
+            rut.value = cuerpo + '-'+ dv
+            
+            // Si no cumple con el mínimo ej. (n.nnn.nnn)
+            if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+            
+            // Calcular Dígito Verificador
+            suma = 0;
+            multiplo = 2;
+            
+            // Para cada dígito del Cuerpo
+            for(i=1;i<=cuerpo.length;i++) {
+            
+                // Obtener su Producto con el Múltiplo Correspondiente
+                index = multiplo * valor.charAt(cuerpo.length - i);
+                
+                // Sumar al Contador General
+                suma = suma + index;
+                
+                // Consolidar Múltiplo dentro del rango [2,7]
+                if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+        
+            }
+            
+            // Calcular Dígito Verificador en base al Módulo 11
+            dvEsperado = 11 - (suma % 11);
+            
+            // Casos Especiales (0 y K)
+            dv = (dv == 'K')?10:dv;
+            v = (dv == 0)?11:dv;
+            
+            // Validar que el Cuerpo coincide con su Dígito Verificador
+            if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+            
+            // Si todo sale bien, eliminar errores (decretar que es válido)
+            rut.setCustomValidity("RUT válido");
+            //rut.setCustomValidity('');
+        }
+
+</script>
+
+<!-- ./ VENTANA MODAL PARA REGISTRO Y ACTUALIZACION -->
 <script>
 
 	$(document).ready(function(){
 
-        $.ajax({
-            url: "ajax/practicante.ajax.php",
-                        method: "GET",
-                        cache: false,
-                        contentType: false,
-                        processData: false,
-                        success: function(respuesta){
-                            console.log("Datos"+respuesta+"Fin de DAtos");
-                        }
-        });
+        // $.ajax({
+        //     url: "ajax/practicante.ajax.php",
+        //                 method: "GET",
+        //                 cache: false,
+        //                 contentType: false,
+        //                 processData: false,
+        //                 success: function(respuesta){
+        //                     console.log("Datos"+respuesta+"Fin de DAtos");
+        //                 }
+        // });
 
         var accion = "";
 
@@ -280,7 +534,7 @@
             "columnDefs":[ 
 	           
             		{
-	            		"targets": 11,
+	            		"targets": 8,
 	            		"sortable": false,
 	            		"render": function (data, type, full, meta){
 	            			return "<center>" +
@@ -289,6 +543,9 @@
 	            					    "</button> " + 
 	            					    "<button type='button' class='btn btn-danger btn-sm btnEliminar'> " +
 	            						  "<i class='fas fa-trash'> </i> " +
+	            					    "</button>" + "<b> "+""+"</b> " +
+                                        "<button type='button' class='btn btn-secondary btn-sm btnVer'  data-toggle='modal' data-target='#modal-gestionar-pver'> " +
+	            						  "<i class='fas fa-eye'> </i> " +
 	            					    "</button>" +
 	                                "</center>";
 	                    }
@@ -298,14 +555,14 @@
                     {"data": "ID_PRACTICANTES"},
                     {"data": "NOMBRES"},
                     {"data": "APELLIDOS"},
-                    {"data": "RUT"},
+                    // {"data": "RUT"},
                     {"data": "5"},
                     {"data": "7"},
                     {"data": "9"},
                     {"data": "FECHA_INICIO"},
                     {"data": "FECHA_TERMINO"},
-                    {"data": "FOTO"},
-                    {"data": "14"},
+                    // {"data": "FOTO"},
+                    // {"data": "14"},
                     {"data": "acciones"},
                 ],
 
@@ -490,9 +747,33 @@
             },
   		});
 
+          $('#tablaPracticante tbody').on('click','.btnVer',function(){
+            
+            var data = table.row($(this).parents('tr')).data();
+            accion = "actualizar";
+
+                            $("#idpracticante").val(data["ID_PRACTICANTES"]),
+                            $("#txtnonbre").val(data["NOMBRES"]),
+                            $("#txtapellido").val(data["APELLIDOS"]),
+                            $("#ru").val(data["RUT"]),
+                            $("#Sinstitucio").val(data["4"]),
+                            $("#Scarrer").val(data["6"]),
+                            $("#Stipo_practic").val(data["8"]),
+                            $("#fecha_inici").val(data["FECHA_INICIO"]),
+                            $("#fecha_termin").val(data["FECHA_TERMINO"]),
+                            $("#txtfot").val(data["FOTO"]),
+                            $("#Sencargad").val(data["13"]);
+                            console.log("Imprimiendo "+data["NOMBRES"]);
+
+        })
+
         $(".btn-agregar-practicantes").on('click',function(){
             accion = "registrar";
         });
+
+
+
+
 
         $('#tablaPracticante tbody').on('click','.btnEliminar',function(){
             var data = table.row($(this).parents('tr')).data();
@@ -553,7 +834,7 @@
                             $("#idpracticantes").val(data["ID_PRACTICANTES"]),
                             $("#txtnonbres").val(data["NOMBRES"]),
                             $("#txtapellidos").val(data["APELLIDOS"]),
-                            $("#txtrut").val(data["RUT"]),
+                            $("#rut").val(data["RUT"]),
                             $("#Sinstitucion").val(data["4"]),
                             $("#Scarrera").val(data["6"]),
                             $("#Stipo_practica").val(data["8"]),
@@ -571,7 +852,7 @@
             var id = $("#idpracticantes").val(),
                 nombres = $("#txtnonbres").val(),
                 apellidos = $("#txtapellidos").val(),
-                rut = $("#txtrut").val(),
+                rut = $("#rut").val(),
                 institucion_id = $("#Sinstitucion").val(),
                 carrera_id = $("#Scarrera").val(),
                 tipo_practica_id = $("#Stipo_practica").val(),
@@ -580,7 +861,7 @@
                 foto = $("#txtfoto").val(),
                 encargado_id = $("#Sencargado").val();
 
-                //console.warn("Imprimiendo "+foto);
+                console.warn("Imprimiendo "+rut);
 
                 //fecha = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
             
@@ -636,7 +917,7 @@
                             $("#idpracticantes").val(""),
                             $("#txtnonbres").val(""),
                             $("#txtapellidos").val(""),
-                            $("#txtrut").val(""),
+                            $("#rut").val(""),
                             $("#Sinstitucion").val(""),
                             $("#Scarrera").val(""),
                             $("#Stipo_practica").val(""),
